990342b8bf4280f561eb34b3ef9251afa00338bd
Athaba2016 <sumiefb@gmail.com>
2023-07-13 19:03:32 -0700

    hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+  * main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a pie of art,\n");
+	return (0);
