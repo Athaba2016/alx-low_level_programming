cec3df8347e65b5b5dd009d11f4b827ded574c1e
Athaba2016 <sumiefb@gmail.com>
2023-07-25 12:37:21 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "unistd.h"
 /**
  * puts2 - function should print only one character out of two
  * starting with the first one
@@ -26,16 +27,4 @@ void puts2(char *str)
 	}
 	}
 	_putchar('\n');
-}
-
-
-
-
-
-
-
-
-
-
-
 }
